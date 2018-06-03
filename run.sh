19773ccd60b7cbe28405b1b869237fa0efaf77e4
Liang Shuailong <liangshuailong@gmail.com>
2018-06-03 23:11:49 +0800

    add run scripts

---
+++ run.sh
@@ -0,0 +1,6 @@
+# @Author: Liang Shuailong
+# @Date:   2018-06-01 17:45:48
+# @Last Modified by:   Liang Shuailong
+# @Last Modified time: 2018-06-01 17:49:54
+
+python script/blameextract/train.py --bidirectional False --pooling rand
